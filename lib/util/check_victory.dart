a7ca40206da738a7f4b42fef669876255bd2b15a
Fabian Varela Bonett <jfabian.303@gmail.com>
2019-04-02 11:28:41 -0500

    Create the "AI" to check what is the winner player

---
+++ lib/util/check_victory.dart
@@ -0,0 +1,69 @@
+import 'package:tic_tac_toe_flutter/model/victory.dart';
+import 'package:tic_tac_toe_flutter/util/constants.dart';
+
+class CheckVictory {
+  static Victory checkForVictory(List<List<String>> field, String playerChar) {
+    Victory v;
+
+    //check horizontal lines
+    if (field[0][0].isNotEmpty &&
+        field[0][0] == field[0][1] &&
+        field[0][0] == field[0][2]) {
+      v = Victory(
+          0, 0, HORIZONTAL, field[0][0] == playerChar ? PLAYER : AI_PLAYER);
+    } else if (field[1][0].isNotEmpty &&
+        field[1][0] == field[1][1] &&
+        field[1][0] == field[1][2]) {
+      v = Victory(
+          1, 0, HORIZONTAL, field[1][0] == playerChar ? PLAYER : AI_PLAYER);
+    } else if (field[2][0].isNotEmpty &&
+        field[2][0] == field[2][1] &&
+        field[2][0] == field[2][2]) {
+      v = Victory(
+          2, 0, HORIZONTAL, field[2][0] == playerChar ? PLAYER : AI_PLAYER);
+    }
+
+    //check vertical lines
+    else if (field[0][0].isNotEmpty &&
+        field[0][0] == field[1][0] &&
+        field[0][0] == field[2][0]) {
+      v = Victory(
+          0, 0, VERTICAL, field[0][0] == playerChar ? PLAYER : AI_PLAYER);
+    } else if (field[0][1].isNotEmpty &&
+        field[0][1] == field[1][1] &&
+        field[0][1] == field[2][1]) {
+      v = Victory(
+          0, 1, VERTICAL, field[0][1] == playerChar ? PLAYER : AI_PLAYER);
+    } else if (field[0][2].isNotEmpty &&
+        field[0][2] == field[1][2] &&
+        field[0][2] == field[2][2]) {
+      v = Victory(
+          0, 2, VERTICAL, field[0][2] == playerChar ? PLAYER : AI_PLAYER);
+    }
+
+    //check diagonal
+    else if (field[0][0].isNotEmpty &&
+        field[0][0] == field[1][1] &&
+        field[0][0] == field[2][2]) {
+      v = Victory(0, 0, DIAGONAL_DESCENDING,
+          field[0][0] == playerChar ? PLAYER : AI_PLAYER);
+    } else if (field[2][0].isNotEmpty &&
+        field[2][0] == field[1][1] &&
+        field[2][0] == field[0][2]) {
+      v = Victory(2, 0, DIAGONAL_ASCENDING,
+          field[2][0] == playerChar ? PLAYER : AI_PLAYER);
+    } else if (field[0][0].isNotEmpty &&
+        field[0][1].isNotEmpty &&
+        field[0][2].isNotEmpty &&
+        field[1][0].isNotEmpty &&
+        field[1][1].isNotEmpty &&
+        field[1][2].isNotEmpty &&
+        field[2][0].isNotEmpty &&
+        field[2][1].isNotEmpty &&
+        field[2][2].isNotEmpty) {
+      v = Victory(-1, -1, -1, DRAFT);
+    }
+
+    return v;
+  }
+}
